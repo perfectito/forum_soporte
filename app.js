9ed55ae29f5472f491788513e6535aa089cc849b
perfectito <129255589+perfectito@users.noreply.github.com>
2023-04-21 15:59:05 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+document.getElementById("boton1").addEventListener("click", function() {
+  window.open("https://docs.google.com/forms/d/e/1FAIpQLScHj3x7XAJL0qVYOEFZjgAvfKtfxgYpC4oLKGLxKnRsQ96IFg/viewform", "_blank");
+});
+
+document.getElementById("boton2").addEventListener("click", function() {
+  window.open("https://docs.google.com/forms/d/e/1FAIpQLScnCg4lHgP3R1m7TdyEmjbYc36Omu9ORUFs3CjMQNeFkkRRag/viewform", "_blank");
+});
+
+document.getElementById("boton3").addEventListener("click", function() {
+  window.open("https://docs.google.com/forms/d/e/1FAIpQLSdtO7kvUy1RVtuHI7MpVf89L6Wr7YxGR77oW9pgGpe0duqY-g/viewform", "_blank");
+});
+
+document.getElementById("boton4").addEventListener("click", function() {
+  window.open("https://docs.google.com/spreadsheets/d/1AQ1vCSx7An_o6ecyK_6xWfIieP-79TO3n0FJ3qGMAa4/edit#gid=0", "_blank");
+});
+
+document.getElementById("boton5").addEventListener("click", function() {
+  window.open("https://keep.google.com/share?note=1odqaULOyQXe-LQCMO1fH_xMI9NLtgC1uJQmsdDxK5mugSUBsw4todAmPXwniMJ8L&email=rbengoa@gmail.com&tid=true");
+});
+
+document.getElementById("boton6").addEventListener("click", function() {
+  window.open("https://logicamente.webcindario.com/Util%20Forum/index.html", "_blank");
+});
